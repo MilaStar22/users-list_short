9a6437dffad2123d885c3812fa92604fc50f1244
Mila_Star <101258091+MilaStar22@users.noreply.github.com>
2024-05-10 16:46:10 +0200

    added sorting to list

--- users-list/src/app/components/users-list/users-list.component.ts
+++ users-list/src/app/components/users-list/users-list.component.ts
@@ -1,110 +1,47 @@
-import { Component } from '@angular/core';
-import { User } from './user.interface';
-import { MatListModule } from '@angular/material/list';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../interfaces/user.interface';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [MatListModule],
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatIconModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
-export class UsersListComponent {
-  usersList: User[] = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz",
-      "role": "Admin",
-      "phone": "1-770-736-8031 x56442",
-      "website": "hildegard.org"
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv",
-      "role": "Admin",
-      "phone": "010-692-6593 x09125",
-      "website": "anastasia.net"
-    },
-    {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "username": "Samantha",
-      "email": "Nathan@yesenia.net",
-      "role": "User",
-      "phone": "1-463-123-4447",
-      "website": "ramiro.info"
-    },
-    {
-      "id": 4,
-      "name": "Patricia Lebsack",
-      "username": "Karianne",
-      "email": "Julianne.OConner@kory.org",
-      "role": "User",
-      "phone": "493-170-9623 x1567",
-      "website": "kale.biz"
-    },
+export class UsersListComponent implements OnInit {
+  usersList: User[] = [];
+  foods: Food[] = [
+    { value: 'sort-0', viewValue: 'A - Z' },
+    { value: 'sort-1', viewValue: 'Z - A' }
+  ];
 
-    {
-      "id": 5,
-      "name": "Chelsey Dietrich",
-      "username": "Kamren",
-      "email": "Lucio_Hettinger@annie.ca",
-      "role": "User",
-      "phone": "(254)954-1289",
-      "website": "demarco.info"
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  protected readonly isNameFound: boolean = false;
 
-    },
-    {
-      "id": 6,
-      "name": "Mrs. Dennis Schulist",
-      "username": "Leopoldo_Corkery",
-      "email": "Karley_Dach@jasper.info",
-      "role": "Admin",
-      "phone": "1-477-935-8478 x6430",
-      "website": "ola.org"
+  constructor(public usersService: UserService) { }
 
-    },
-    {
-      "id": 7,
-      "name": "Kurtis Weissnat",
-      "username": "Elwyn.Skiles",
-      "email": "Telly.Hoeger@billy.biz",
-      "role": "Admin",
-      "phone": "210.067.6132",
-      "website": "elvis.io"
+  ngOnInit(): void {
+    this.usersList = this.usersService.getUsersList();
+  }
 
-    },
-    {
-      "id": 8,
-      "name": "Nicholas Runolfsdottir",
-      "username": "Maxime_Nienow",
-      "email": "Sherwood@rosamond.me",
-      "role": "User",
-      "phone": "586.493.6943 x140",
-      "website": "jacynthe.com"
-    },
-    {
-      "id": 9,
-      "name": "Glenna Reichert",
-      "username": "Delphine",
-      "email": "Chaim_McDermott@dana.io",
-      "role": "User",
-      "phone": "(775)976-6794 x41206",
-      "website": "conrad.com"
-    },
-    {
-      "id": 10,
-      "name": "Clementina DuBuque",
-      "username": "Moriah.Stanton",
-      "email": "Rey.Padberg@karina.biz",
-      "role": "User",
-      "phone": "024-648-3804",
-      "website": "ambrose.net"
-    }
-  ]
+  search(query: string) {
+    return this.usersList = this.usersService.findUserName(query);
+  }
 
+  sortByMethod(query: string) {
+    const direction = (query === 'sort-0') ? 1 : -1;
+    return this.usersList.sort((a, b) => direction * ((a.username > b.username) ? 1 : -1));
+  }
 }
